4c2b3e87468e6f493a8180a659cfcf55c5ad08b2
Daniel111-sourse <d8003635@gmail.com>
2025-11-15 14:17:00 +0300

    init

--- script.js
+++ script.js
@@ -68,11 +68,49 @@ function resetGame((ball.x < 0 ||  ball.x > map.width) && !ball.isReseted) {
             ball.x = map.width / 2;
             ball.y = map.height / 2;
             ball.isResetted = false;
-        }, 1000);
+        }, 2000);
     }
 }
-
-
+function collideWallsWithPaddle(paddle) {
+    if (paddle.y < grid) {
+        paddle.y = grid;
+    }
+    else if (paddle.y > maxPaddleY) {
+        paddle.y = maxPaddleY;
+    }
+}
+function collideWalsWithPaddle(){
+    collideWallsWithPaddle(leftPaddle)
+    collideWallsWithPaddle(rightPaddle)
+}
+function isCollides(object1, object2) {
+    const width1 = object1.x + object1.width;
+    const width2 = object2.x + object2.width;
+    const height1 = object1.y + object1.height;
+    const height2 = object2.y + object2.height;
+    return object1.x < width2
+        && object2.x < width1
+        && object1.y < height2
+        && object2.y < height1;
+}
+function collideWallsWithBall() {
+    if (ball.y < grid) {
+        ball.y = grid;
+        ball.dy = -ball.dy;
+    }
+    else if (ball.y > map.height - grid) {
+        ball.y = map.height - grid;
+        ball.dy = -ball.dy;
+    }
+        if (isCollides(ball, rightPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = rightPaddle.x - ball.width;
+    }
+    else if (isCollides(ball, leftPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = leftPaddle.x + leftPaddle.width;
+    }
+}
 function loop (){
     clearMap()
     renderMap()
@@ -81,6 +119,8 @@ function loop (){
     renderBall()
     moveBall()
     movePaddles()
+    collideWalsWithPaddle()
+    collideWalsWithBall()
     resetGame()
     requestAnimationFrame(loop)
 }
@@ -99,3 +139,4 @@ document.addEventListener('keyup', (event) => {
 })
 
 requestAnimationFrame(loop)
+
